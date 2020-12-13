d31d1816db451a213918ca77b4437189a1207e8b
Abdul Wajid <49652572+AbdulWajid99@users.noreply.github.com>
2020-12-13 23:00:06 +0500

    AL game started

---
+++ Tic-Tac-Toe_AI.py
@@ -0,0 +1,22 @@
+# Base Board
+
+board = [' ' for x in range[10]]
+
+
+def insetLetter(letter, pos):
+    board[pos] = letter
+
+
+def spaceFree(pos):
+    return board(pos)
+
+# %%
+
+
+def printBoard(board):
+    print('    |    |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3]+' | ')
+    print('    |    |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6]+' | ')
+    print('    |    |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9]+' | ')
